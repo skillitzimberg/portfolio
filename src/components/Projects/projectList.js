0217b08cd5fc1b4869653913e18dfa68c4bd4b5e
Scott Bergler <commandinghands@gmail.com>
2019-02-15 12:56:16 -0800

    add coding and music projects to projectList.js.

--- src/components/Projects/projectList.js
+++ src/components/Projects/projectList.js
@@ -1,17 +1,34 @@
-export const projectList = [
+export const codingList = [
   {
     name: "jYoukBox",
-    url: "August 21, 2018",
-    description: "This is an description."
+    url: "https://github.com/skillitzimberg/JYoukBox",
+    description: "A music player using the YouTube API to make a playlist of videos."
   },
   {
     name: "Trimet Commuter",
-    url: "August 22, 2018",
-    description: "This is another description."
+    url: "https://github.com/skillitzimberg/trimet-commuter",
+    description: "TriMet Commuter is a light TriMet transit tool. Enter your daily morning and evening stops."
   },
   {
-    name: "Template JS",
-    url: "August 23, 2018",
-    description: "This is yet another description."
+    name: "Tumbelweed Bakehouse",
+    url: "https://github.com/skillitzimberg/TumbleweedBakehouse.Solution",
+    description: "The first step in creating a website/app to help my sister manage her bakery business by allowing her customers to place orders online, storing order information in a database, allowing specific products to be displayed as appropriate, and reducing manual (pen & paper) data handling"
+  }
+];
+export const otherProjectsList = [
+  {
+    name: "Damn Family",
+    url: "https://damnfamily.bandcamp.com/",
+    description: "All-acoustic old-school country & bluegrass music played by people with backgrounds in jazz, country, metal, punk & noise."
+  },
+  {
+    name: "Roxy Epoxy",
+    url: "https://www.metropolis-records.com/artist/roxy-epoxy--the-rebound",
+    description: "Roxy Epoxy's band post-Epoxies. Maybe Goth Glam Metal New Wave?"
+  },
+  {
+    name: "Miru Mir",
+    url: "http://www.barynya.com/russianmusic/gypsy_band_portland_oregon.htm",
+    description: "Russian rock & roll band that is currently buried in the digital sands of time (i.e. I can't find any of our music online)."
   }
 ];
