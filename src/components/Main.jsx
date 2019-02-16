955d488f903a917abe9e5fdd34f81cc701cf5ae2
Scott Bergler <commandinghands@gmail.com>
2019-02-15 20:26:51 -0800

    add typography, padding, & image to header.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,15 @@ import Header from "./Header";
 
 function Main(){
   return (
-    <div className="container">
+    <div>
       <Header pageHead={"portfolio"}/>
-      <main>
-        <p>Statement of Intent I guess . . . Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </main>
+      <div className="container">
+        <main>
+          <p>Statement of Intent I guess . . . Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,48 +2,49 @@ import React from "react";
 import Header from "../Header";
 
 function Resume(){
-  
+
   return (
-    <div className="container">
-      <Header pageHead={"Scott Bergler"}/>
-      <div className="header">
-        <h3>commandinghands@gmail.com :: 555.555.5555</h3>
+    <div>
+    <Header pageHead={"Scott Bergler"}/>
+      <div className="container">
+        <div className="header">
+          <h3>commandinghands@gmail.com :: 555.555.5555</h3>
+        </div>
+
+        <div className="objective">
+          <p>Get job. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div className="work">
+          <p className="color">company <span>dates</span></p>
+          <p>duties</p>
+          <ul>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          </ul>
+
+          <p>company <span>dates</span></p>
+          <p>duties</p>
+          <ul>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          </ul>
+
+          <p>company <span>dates</span></p>
+          <p>duties</p>
+          <ul>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          <li>stuff and things</li>
+          </ul>
+        </div>
+
       </div>
-
-      <div className="objective">
-        <p>Get job. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-
-      <div className="work">
-        <p className="color">company <span>dates</span></p>
-        <p>duties</p>
-        <ul>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-        </ul>
-
-        <p>company <span>dates</span></p>
-        <p>duties</p>
-        <ul>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-        </ul>
-
-        <p>company <span>dates</span></p>
-        <p>duties</p>
-        <ul>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-        </ul>
-
-      </div>
-
     </div>
   );
 }
