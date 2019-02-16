9e8ea9af4d2d77a898679ef69c990dd5b9f4a67b
Scott Bergler <commandinghands@gmail.com>
2019-02-15 20:29:12 -0800

    fix header width.

--- src/components/Blog/Articles.jsx
+++ src/components/Blog/Articles.jsx
@@ -7,18 +7,20 @@ import "./blogStyles.css";
 function Articles(){
 
   return (
-    <div className="container">
+    <div>
       <Header pageHead={"Brain Dump"}/>
-      <main>
-        <h3>A web development newbie becoming not a newbie.</h3>
-  
-        <section className="articleList">
-          <h3>Blog Posts</h3>
-          {postList.map((post, index) =>
-            <p><Link to="/" key={index}>{post.date}</Link></p>
-          )}
-        </section>
-      </main>
+      <div className="container">
+        <main>
+          <h3>A web development newbie becoming not a newbie.</h3>
+    
+          <section className="articleList">
+            <h3>Blog Posts</h3>
+            {postList.map((post, index) =>
+              <p><Link to="/" key={index}>{post.date}</Link></p>
+            )}
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
