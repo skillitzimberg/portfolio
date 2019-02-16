8f95663b92b5159e6fd5c95859244d2b4d99467d
Scott Bergler <commandinghands@gmail.com>
2019-02-15 20:31:57 -0800

    fix header width.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,35 +8,37 @@ import "./projectStyles.css";
 function Projects(){
 
   return (
-    <div className="container">
+    <div>
       <Header pageHead={"Projects"}/>
-      <main>
-        <section className="codingProjects">
-          <h3>Coding Projects</h3>
-          {codingProjects.map((project, index) =>
-            <Project 
-              name={project.name}
-              url={project.url}
-              description={project.description}
-              image={project.image}
-              key={index}
-            />
-          )}
-        </section>
-  
-        <section className="musicProjects">
-          <h3>Music Projects</h3>
-          {musicProjects.map((project, index) =>
-            <Project 
-              name={project.name}
-              url={project.url}
-              description={project.description}
-              index={project.index}
-              key={index}
-            />
-          )}
-        </section>
-      </main>
+      <div className="container">
+        <main>
+          <section className="codingProjects">
+            <h3>Coding Projects</h3>
+            {codingProjects.map((project, index) =>
+              <Project 
+                name={project.name}
+                url={project.url}
+                description={project.description}
+                image={project.image}
+                key={index}
+              />
+            )}
+          </section>
+      
+          <section className="musicProjects">
+            <h3>Music Projects</h3>
+            {musicProjects.map((project, index) =>
+              <Project 
+                name={project.name}
+                url={project.url}
+                description={project.description}
+                index={project.index}
+                key={index}
+              />
+            )}
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
