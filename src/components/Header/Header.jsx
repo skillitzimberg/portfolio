083671166176c02b4b4aab04725d18bacb62f871
Scott Bergler <commandinghands@gmail.com>
2019-02-15 21:55:57 -0800

    begin styling resume content.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Nav from "./Nav";
+import "./headerStyles.css";
 
 function Header(props){
   return (
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,47 +1,60 @@
 import React from "react";
 import Header from "../Header/Header";
+import "./resumeStyles.css";
 
 function Resume(){
 
   return (
     <div>
-    <Header pageHead={"Scott Bergler"}/>
+      <Header pageHead={"Scott Bergler"}/>
       <div className="container">
-        <div className="header">
-          <h3>commandinghands@gmail.com :: 555.555.5555</h3>
-        </div>
 
-        <div className="objective">
-          <p>Get job. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="resumeHead">
+          <p>commandinghands@gmail.com</p>
+          <p>503.890.5118</p>
+          <p>https://www.linkedin.com/in/scott-bergler/</p> 
+          <p>https://github.com/skillitzimberg/</p>
         </div>
 
         <div className="work">
-          <p className="color">company <span>dates</span></p>
-          <p>duties</p>
-          <ul>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          </ul>
+          <h4>PROFESSIONAL EXPERIENCE</h4>
+          <div className="employer">
+            <p className="job">
+            • Schoolhouse Electric &amp; Supply Company •
+            </p>
+            <span>A Portland, OR born lighting and lifestyle goods designer and manufacturer.</span>
+            <span className="jobTitle">Assembler</span>
+            <ul>
+              <li>Accurately check-in and inspect components in assembly kits.</li>
+              <li>Assemble, test, and package light fixtures and other merchandise.</li>
+              <li>Maintain quality control standards.</li>
+              <li>Maintain up-to-date knowledge of standard operating procedures.</li>
+            </ul>
+          </div>
 
-          <p>company <span>dates</span></p>
-          <p>duties</p>
-          <ul>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          </ul>
+          <div className="employer">
+            <h3 className="color">Schoolhouse Electric &amp; Supply Company<span> • November 2017 – present</span> • Portland, OR</h3>
+            <span>A Portland, OR born lighting and lifestyle goods designer and manufacturer.</span>
+            <span className="jobTitle">Assembler</span>
+            <ul>
+              <li>Accurately check-in and inspect components in assembly kits.</li>
+              <li>Assemble, test, and package light fixtures and other merchandise.</li>
+              <li>Maintain quality control standards.</li>
+              <li>Maintain up-to-date knowledge of standard operating procedures.</li>
+            </ul>
+          </div>
 
-          <p>company <span>dates</span></p>
-          <p>duties</p>
-          <ul>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          <li>stuff and things</li>
-          </ul>
+          <div className="employer">
+            <h3 className="color">Schoolhouse Electric &amp; Supply Company<span> • November 2017 – present</span> • Portland, OR</h3>
+            <span>A Portland, OR born lighting and lifestyle goods designer and manufacturer.</span>
+            <span className="jobTitle">Assembler</span>
+            <ul>
+              <li>Accurately check-in and inspect components in assembly kits.</li>
+              <li>Assemble, test, and package light fixtures and other merchandise.</li>
+              <li>Maintain quality control standards.</li>
+              <li>Maintain up-to-date knowledge of standard operating procedures.</li>
+            </ul>
+          </div>
         </div>
 
       </div>
