0cc7c3cdd6729686450b8b51b94a1db8893533a2
Scott Bergler <commandinghands@gmail.com>
2019-02-15 20:37:48 -0800

    move Header & Nav to /Header.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../Header";
+import Header from "../Header/Header";
 import Project from "./Project";
 import { codingProjects } from "./projectList";
 import { musicProjects } from "./projectList";
