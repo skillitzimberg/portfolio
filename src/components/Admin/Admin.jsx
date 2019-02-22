b5fd3c8f8edd2b377f033da14691a1fbb57945c2
Scott Bergler <commandinghands@gmail.com>
2019-02-22 15:19:12 -0800

    add proptypes to Admin & correct improper placement of props declarations.

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Header from "../Header/Header";
 // import { postList } from "./postList";
 
@@ -6,16 +7,16 @@ function Admin() {
 
   return (
     <div>
-    <Header pageHead={"Admin"} />
-    <Articles />
-    <NewPostForm onNewPostCreation={props.onNewPostCreation}/>
+      <Header pageHead={"Admin"} />
+      <Articles />
+      <NewPostForm onNewPostCreation={props.onNewPostCreation}/>
     </div>
   );
 
-  Admin.propTypes = {
-    onNewPostCreation: PropTypes.func
-  };
-
 }
+
+Admin.propTypes = {
+  onNewPostCreation: PropTypes.func
+};
 
 export default Admin;
