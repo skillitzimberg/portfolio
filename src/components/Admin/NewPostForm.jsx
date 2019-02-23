54b9312499bacc476683f6487f505b2de817e9be
Scott Bergler <commandinghands@gmail.com>
2019-02-23 08:40:55 -0800

    fix linting errors (except CSS) site-wide.

--- src/components/Admin/NewPostForm.jsx
+++ src/components/Admin/NewPostForm.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { v4 } from 'uuid';
-import Moment from 'moment';
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+import Moment from "moment";
 
 function NewPostForm(props){
   let _title = null;
@@ -12,23 +12,23 @@ function NewPostForm(props){
 
     props.onNewPostCreation({title: _title.value, date: new Moment(), article: _article.value, id: v4()});
 
-    _title.value = '';
-    _article.value = '';
+    _title.value = "";
+    _article.value = "";
   }
 
   return (
     <div>
       <form onSubmit={handleNewPostFormSubmission}>
         <input
-          type='text'
-          id='title'
-          placeholder='Article Title'
+          type="text"
+          id="title"
+          placeholder="Article Title"
           ref={(input) => {_title = input;}}/>
         <textarea
-          id='article'
+          id="article"
           placeholder="What's the story?"
           ref={(textarea) => {_article = textarea;}}/>
-        <button type='submit'>Add to Blog</button>
+        <button type="submit">Add to Blog</button>
       </form>
     </div>
   );
--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Moment from 'moment';
+import Moment from "moment";
 
 function Post(props){
   return (
